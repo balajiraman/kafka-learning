51561cd9177334be9159107908f57f93d30c4d85
bvenkataraman <bvenkataraman@RCVU-ENTARC01.cmgmi.local>
2019-04-23 13:42:29 -0700

    intial code text

---
+++ kafka-tutorial/src/main/java/org/balajiraman/tutorial/kafka/ProducerDemo.java
@@ -0,0 +1,10 @@
+package org.balajiraman.tutorial.kafka;
+
+public class ProducerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+	}
+
+}
